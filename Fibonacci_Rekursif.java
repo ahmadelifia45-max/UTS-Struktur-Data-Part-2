db4ad09db805063864fea441ed46cc75afc44522
ahmadelifia45-max <ahmadelifia45@gmail.com>
2026-04-22 01:09:40 +0700

    Add files via upload

---
+++ Fibonacci_Rekursif.java
@@ -0,0 +1,27 @@
+package uts_part2;
+import java.util.Scanner;
+public class Fibonacci_Rekursif {
+    // Fungsi rekursif Fibonacci
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah suku Fibonacci: ");
+        int n = input.nextInt();
+        System.out.println("Deret Fibonacci hingga suku ke-" + n + " adalah:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i));
+            if (i < n - 1) {
+                System.out.print(", ");
+            }
+        }
+        input.close();
+    }
+}
